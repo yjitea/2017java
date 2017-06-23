afe4ee77d803dfc2ee939d2cb36c1f38425d876e
Administrator <Administrator@samnote>
2017-06-24 00:10:27 +0900

    응용컴퓨터 실습 과제 1,2 업데이트 했습니다. (2016.06.24/12:10AM)

---
+++ 응용컴퓨터프로그래밍/src/응용컴퓨터프로그래밍/HomeWork1.java
@@ -0,0 +1,41 @@
+package 응용컴퓨터프로그래밍;
+
+import java.util.Scanner;
+
+public class HomeWork1 {
+	
+	public static int[] FindMax(int []point){
+		for (int j = 1; j < point.length; j++) {
+			int key = point[j];
+			int i = j - 1;
+			while (i >= 0 && point[i] > key) {
+				point[i + 1] = point[i];
+				i--;
+			}
+			point[i + 1] = key;
+		  }
+			return point;
+	    }
+		
+	public static boolean isEnableTriangle(int a,int b,int c){
+		if(a<b+c)
+			return true;
+		else
+			return false;	
+	}
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int a=input.nextInt();
+		int b=input.nextInt();
+		int c=input.nextInt();
+		int []line={a,b,c};
+		FindMax(line); // 삽입정렬 활용하여 line 배열 정렬 (오름차순)
+		if(isEnableTriangle(line[2],line[1],line[0])==true)// 큰 수부터 입력되어 삼각형 판별
+			System.out.println("Input: "+a+" "+b+" "+c+"-> Output: "+'"'+"삼각형을 만들 수 있음."+'"');
+		else
+			System.out.println("Input: "+a+" "+b+" "+c+"-> Output: "+'"'+"삼각형을 만들 수 없음."+'"');
+	}
+}
+ // 위 코드는 입력의 순서 상관없이 삼각형을 판별 해줍니다.
+ // 삼각형의 조건 : 두변의 합은 나머지 한변(가장 큰수) 보다 무조건 커야한다.
+
---
+++ 응용컴퓨터프로그래밍/src/응용컴퓨터프로그래밍/HomeWork2.java
@@ -0,0 +1,54 @@
+package 응용컴퓨터프로그래밍;
+
+import java.util.Scanner;
+
+public class HomeWork2 {
+	
+	public static double[] FindMax(double []point){
+		for (int j = 1; j < point.length; j++) {
+			double key = point[j];
+			int i = j - 1;
+			while (i >= 0 && point[i] > key) {
+				point[i + 1] = point[i];
+				i--;
+			}
+			point[i + 1] = key;
+		  }
+			return point;
+	    }
+		
+	public static boolean isEnableTriangle(double a,double b,double c){
+		if(a<b+c)
+			return true;
+		else
+			return false;	
+	}
+	public static double Length(double a, double b,double c,double d){ // 점 사이의 거리를 구하는 함수 
+	   double Length=Math.sqrt((a-c)*(a-c)+(b-d)*(b-d));
+	   return Length;
+	}
+	public static double triangle(double a,double b,double c,double d,double e,double f){ // 삼각형 넓이를 구해주는 함수
+		double Area=0.5*((a*d+c*f+e*b)-(c*b+e*b+a*f));
+		if (Area<0)
+			return -Area;// 음수일 경우 양수로 전환
+		return Area;
+	}
+	 
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int a=input.nextInt();
+		int b=input.nextInt();
+		int c=input.nextInt();
+		int d=input.nextInt();
+		int e=input.nextInt();
+		int f=input.nextInt();
+		double []line={Length(a,b,c,d),Length(c,d,e,f),Length(e,f,a,b)};
+		FindMax(line);
+		if(isEnableTriangle(line[2],line[1],line[0])==true)
+			System.out.println("Input: "+a+" "+b+" "+c+" "+d+" "+e+" "+f+"-> Output: "+'"'+"삼각형의 면적은"+triangle(a,b,c,d,e,f)+"입니다"+'"');
+		else 
+			System.out.println("Input: "+a+" "+b+" "+c+" "+d+" "+e+" "+f+"-> Output: "+'"'+"삼각형을 만들 수 없음."+'"');
+	}
+}
+ // 삼각형이 될 수 있는지를 판별 하려면 일단 각 변의 길이를 구해야 합니다. 
+ //그래서 'Length' 함수를 이용하여 길이를 구한다음, 'isEnableTriangle' 함수를 이용하여 조건 확인 후 삼각형의 넓이를 구하였습니다.
